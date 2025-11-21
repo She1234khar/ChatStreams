e729c220dba2c620530a775a1ec06c3562b136d3
shekhar kumar <shekhar2004kumar@gmail.com>
2025-11-21 09:13:13 +0530

    she

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://chatstreams-backend.onrender.com/api";
+    : "/api";
 
 
 export const axiosInstance = axios.create({
